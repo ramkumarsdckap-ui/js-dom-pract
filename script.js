6bbe69021e03b4120dab97e114c36092330be1ba
ramkumarsdckap-ui <ramkumarsdckap@gmail.com>
2025-11-20 11:44:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1
+
+// function string(a,b){
+// for(i=0;i<a.length;i++){
+//   if(a[i]==b){
+     
+//       console.log(`True, And the index is ${a.indexOf(b)}`)
+//   }
+// }
+// }
+// string(["org","hii", "abc"],"abc");
+
+
+
+//2.above three true or false
+
+// let arr=[3,5,1,2,9]
+// let b=[]
+// function odd(){
+//     for (i=0;i<arr.length;i++){
+//         if(arr[i]%2!== 0){
+//             b[b.length]=arr[i]
+//         }
+//     }
+//             if(b.length>3){
+//             console.log("True")
+//         }
+//             else{
+//               console.log("False")  
+//             }
+// }
+// odd();
+
+
+
+//3.diffrent array
+
+// let a=[1,2,3];
+// let b=[3,4,5];
+// let c=[]
+// for (i=0;i<arr.length;i++){
+//     for (j=0;j<arr.length;j++){
+//         if(a[i]==b[j]){
+//          c[c.length]=a[i]
+//         }
+//     }
+// }
+// console.log(c)
